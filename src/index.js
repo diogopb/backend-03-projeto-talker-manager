0719b9b8bb594ff2087e57ce03bd8b5f3bc11aba
Diogo Pinto Barbosa <dpintobarbosa@gmail.com>
2024-04-11 15:51:14 -0300

    Requisito 2 e correção lint

--- src/index.js
+++ src/index.js
@@ -12,8 +12,22 @@ app.get('/talker', async (req, res) => {
 
   if (speakers.length === 0) {
     return res.status(200).json([]);
-  } else {
-    return res.status(200).json(speakers);
+  }
+  return res.status(200).json(speakers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  
+  try {
+    const speakers = JSON.parse(await fs.readFile('src/talker.json', 'utf-8'));
+    const searchSpeaker = speakers.find((speaker) => speaker.id === id);
+    if (!searchSpeaker) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(searchSpeaker);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao buscar palestrante' });
   }
 });
 
